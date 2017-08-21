380166081b918a777625410e94aca3f9f56c1a23
Renato Degiovanni <rdgvnn@gmail.com>
2017-08-21 07:05:32 -0300

    JSON Example with Swift
    I used this as a way to learn Swift, and also some filters to pick up data.

--- JSON/ViewController.swift
+++ JSON/ViewController.swift
@@ -12,7 +12,38 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let url = URL(string: "http://api.fixer.io/latest")
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil
+            {
+                print("Error")
+            }
+            else
+            {
+                if let content = data
+                {
+                    do
+                    {
+                        //Array
+                        let myJSON = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                        if let rates = myJSON["rates"] as? NSDictionary
+                        {
+                            if let currency = rates["GBP"]
+                            {
+                                print(currency)
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        
+                    }
+                }
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
